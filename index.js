aefb0d43b47a9e6e1bb3662f4055c477d1c7bd44
DharaMaradiya <dharumaradiya@gmail.com>
2025-08-05 12:36:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log("dhara")
-const name="Dhara"
-console.log(`Hello ${name}`)
-console.log("Hello from atmiya")
+
+const express=require ("express");
+const stuentRoute=require("./routes/studentRoute");
+const app=express();
+const port=80;
+app.use(express.json());
+
+app.use("/student",studentRoute);
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}/`);
+});
